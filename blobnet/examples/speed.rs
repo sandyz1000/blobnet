5e1fbf327fce91f7d894a614ee240067788f0e53
Eric Zhang <ekzhang1@gmail.com>
2022-07-26 16:58:44 -0400

    Update the speed benchmark

--- blobnet/examples/speed.rs
+++ blobnet/examples/speed.rs
@@ -4,10 +4,7 @@
 //! To use this script, run like the following example:
 //!
 //! ```bash
-//! cargo run --release --example speed \
-//!     http://172.31.21.36:7609 \
-//!     002763c21dd2eaad246b0fd336b045d4965366cbb45e375d277b5e544ae3e8f9 \
-//!     auth-secret
+//! cargo run --release --example speed http://127.0.0.1:7609 secret
 //! ```
 //!
 //! The first argument is the address of the blobnet server, and the second
@@ -23,24 +20,23 @@ use blobnet::FileClient;
 #[tokio::main]
 async fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    assert!(
-        args.len() == 4,
-        "usage: {} <origin> <hash> <secret>",
-        args[0],
-    );
+    assert!(args.len() == 3, "usage: {} <origin> <secret>", args[0]);
 
     let origin = &args[1];
-    let hash = &args[2];
-    let secret = &args[3];
+    let secret = &args[2];
+
+    let data = str::repeat("abcdefghijklmnop", 4096); // 64 KiB
 
     let client = FileClient::new(origin, secret);
-    let output = client.get(hash, None).await?;
+    let hash = client.put(|| async { Ok(data.clone()) }).await?;
+
+    let output = client.get(&hash, None).await?;
     println!("read {} bytes", output.len());
 
     let mut times = vec![];
     for _ in 0..10000 {
         let start = Instant::now();
-        let output2 = client.get(hash, None).await?;
+        let output2 = client.get(&hash, None).await?;
         times.push(start.elapsed().as_micros());
         assert!(output2.len() == output.len());
     }
